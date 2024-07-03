b371cb77e0785f238579ccd3c80a5f6627b0c16b
KcPele <fidekg123@gmail.com>
2024-07-03 10:38:59 +0100

    use state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app__container">
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </div>
+  );
 }
 
 export default App;
