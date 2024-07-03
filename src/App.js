803fc7bc727ce421c9b64892c9304054fbbc1a80
KcPele <fidekg123@gmail.com>
2024-07-03 11:06:04 +0100

    use memo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
   const [dark, setDark] = useState(false);
   // const doubleNumber = slowFunction(number);
+  //solution to avoid re-rendering
   const doubleNumber = useMemo(() => slowFunction(number), [number]);
 
-  const themeStyles = {
-    backgroundColor: dark ? "black" : "white",
-    color: dark ? "white" : "black",
-  };
+  // const themeStyles = {
+  //   backgroundColor: dark ? "black" : "white",
+  //   color: dark ? "white" : "black",
+  // };
+  //solution to avoid re-rendering
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
+
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
   return (
     <>
       <input
