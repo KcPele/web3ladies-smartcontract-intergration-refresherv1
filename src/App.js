e4dd2d4821e5d7a190069ef118931ce3fcb9f6a7
KcPele <fidekg123@gmail.com>
2024-07-03 11:55:47 +0100

    use context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import React from "react";
+import FunctonContextComponent from "./FunctonContextComponent";
+export const ThemeContext = React.createContext();
 function App() {
-  return <div></div>;
+  const [darkTheme, setDarkTheme] = React.useState(false);
+
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+  return (
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <FunctonContextComponent />
+      </ThemeContext.Provider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/FunctonContextComponent.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "./App";
+
+const FunctonContextComponent = () => {
+  return (
+    <div>
+      <h1>Functions</h1>
+    </div>
+  );
+};
+
+export default FunctonContextComponent;
