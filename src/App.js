63571e210e7ce83a98f051501238f0f80f625c7e
KcPele <fidekg123@gmail.com>
2024-07-03 10:47:04 +0100

    use effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
 function App() {
-  return <div></div>;
+  const [resourcesType, setResourcesType] = useState("posts");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowWidth);
+    return () => {
+      window.removeEventListener("resize", updateWindowWidth);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourcesType}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, [resourcesType]);
+  return (
+    <div>
+      <button onClick={() => setResourcesType("posts")}>Posts</button>
+      <button onClick={() => setResourcesType("users")}>Users</button>
+      <button onClick={() => setResourcesType("comments")}>Comments</button>
+
+      <h1>{resourcesType}</h1>
+      <h2>{windowWidth}</h2>
+    </div>
+  );
 }
 
 export default App;
