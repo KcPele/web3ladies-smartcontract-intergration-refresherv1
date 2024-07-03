fd7fe183a2aa4cd6629586f1a119afc819f3a685
KcPele <fidekg123@gmail.com>
2024-07-03 11:13:06 +0100

    use ref

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import React, { useEffect, useState } from "react";
 function App() {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const renderCount = React.useRef(0);
+  const inpurRef = React.useRef();
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>{name}</p>
+      <p>Render count: {renderCount.current}</p>
+    </div>
+  );
 }
 
 export default App;
